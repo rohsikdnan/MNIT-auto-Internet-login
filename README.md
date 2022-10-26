0b654b5c9b94cd7af672e7be3a999758a2ac4ffb
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2022-10-26 23:15:16 +0530

    ain

---
+++ README.md
@@ -0,0 +1,6 @@
+## Steps to be followed
+- download the repository and unzip it
+- open the `script.js` file and put your id and password and save it
+- go to [chrome://extensions/](chrome://extensions/)
+- enable developer options
+- click load unpacked and select your unzipped folder
---
+++ script.js
@@ -0,0 +1,7 @@
+let username = document.querySelector("input[name = auth_user]"),
+    password = document.querySelector("input[name = auth_pass]"),
+    btn = document.querySelector("input[name = accept]");
+
+username.value = '202xxxxxxxx';//id
+password.value = 'xxxxx';//password
+btn.click();
